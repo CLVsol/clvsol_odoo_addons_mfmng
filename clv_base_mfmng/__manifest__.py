c37c1f2d969a2d55c4cafa74de8cd6b1267f67cb
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-02-21 17:29:30 -0300

    [REF] clv_base_mfmng (Migration to version 12.0)

--- clv_base_mfmng/__manifest__.py
+++ clv_base_mfmng/__manifest__.py
@@ -1,29 +1,13 @@
 # -*- coding: utf-8 -*-
-###############################################################################
-#
-# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
+# Copyright (C) 2017-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Base Module (customizations for Media File Management Solution)',
     'summary': 'Base Module Module customizations for Media File Management Solution.',
-    'version': '3.0.0',
+    'version': '12.0.4.0',
     'author': 'Carlos Eduardo Vercelino - CLVsol',
-    'category': 'Generic Modules/Others',
+    'category': 'CLVsol Solutions',
     'license': 'AGPL-3',
     'website': 'https://github.com/CLVsol',
     'depends': [
