0b074d4d6a88ac062b124b112f99ee06c0fc1518
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-02-26 18:45:14 -0300

    [REF] clv_mfile_mfmng (Migration to version 12.0)

--- clv_mfile_mfmng/__manifest__.py
+++ clv_mfile_mfmng/__manifest__.py
@@ -1,27 +1,11 @@
 # -*- coding: utf-8 -*-
-###############################################################################
-#
 # Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Media File (customizations for Media File Management Solution)',
     'summary': 'Media File Module customizations for Media File Management Solution.',
-    'version': '3.0.0',
+    'version': '12.0.4.0',
     'author': 'Carlos Eduardo Vercelino - CLVsol',
     'category': 'Generic Modules/Others',
     'license': 'AGPL-3',
@@ -31,14 +15,14 @@
         'clv_mfile',
     ],
     'data': [
-        'views/mfile_code_view.xml',
-        'views/mfile_reg_state_view.xml',
-        'views/mfile_state_view.xml',
-        'data/mfile_seq_03.xml',
-        'data/mfile_seq_04.xml',
-        'data/mfile_seq_06.xml',
-        'data/mfile_seq_09.xml',
-        'data/mfile_seq_10.xml',
+        # 'views/mfile_code_view.xml',
+        # 'views/mfile_reg_state_view.xml',
+        # 'views/mfile_state_view.xml',
+        # 'data/mfile_seq_03.xml',
+        # 'data/mfile_seq_04.xml',
+        # 'data/mfile_seq_06.xml',
+        # 'data/mfile_seq_09.xml',
+        # 'data/mfile_seq_10.xml',
         'views/mfile_menu_view.xml',
     ],
     'demo': [],
--- clv_mfile_mfmng/models/mfile_code.py
+++ clv_mfile_mfmng/models/mfile_code.py
@@ -1,40 +1,30 @@
 # -*- coding: utf-8 -*-
-###############################################################################
-#
 # Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+# from odoo import api, fields, models
+from odoo import api, models
 
 
 class MediaFile(models.Model):
     _name = "clv.mfile"
-    _inherit = 'clv.mfile', 'clv.code02.model'
+    # _inherit = 'clv.mfile', 'clv.code02.model'
+    _inherit = 'clv.mfile'
 
-    code = fields.Char(string='Media File Code', required=False, default='/')
-    code_sequence_03 = fields.Char(default='clv.mfile.code_03')
-    code_sequence_04 = fields.Char(default='clv.mfile.code_04')
-    code_sequence_06 = fields.Char(default='clv.mfile.code_06')
-    code_sequence_09 = fields.Char(default='clv.mfile.code_09')
-    code_sequence_10 = fields.Char(default='clv.mfile.code_10')
+    # code = fields.Char(string='Media File Code', required=False, default='/')
+    # code_sequence_03 = fields.Char(default='clv.mfile.code_03')
+    # code_sequence_04 = fields.Char(default='clv.mfile.code_04')
+    # code_sequence_06 = fields.Char(default='clv.mfile.code_06')
+    # code_sequence_09 = fields.Char(default='clv.mfile.code_09')
+    # code_sequence_10 = fields.Char(default='clv.mfile.code_10')
 
     @api.one
     def _compute_path_str(self):
         if self.alias:
-            self.path = self.alias + '_' + self.code + '_'
+            if self.code:
+                self.path = self.alias + '_' + self.code + '_'
+            else:
+                self.path = self.alias
         else:
-            self.path = '_' + self.code + '_'
+            if self.code:
+                self.path = '_' + self.code + '_'
