15a47766681e86f3d4dd852e512c7fa26808a573
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-02-27 14:42:17 -0300

    [IMP] clv_mfile_mfmng (Enable code sequences)

--- clv_mfile_mfmng/models/mfile_code.py
+++ clv_mfile_mfmng/models/mfile_code.py
@@ -2,21 +2,19 @@
 # Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-# from odoo import api, fields, models
-from odoo import api, models
+from odoo import api, fields, models
 
 
 class MediaFile(models.Model):
     _name = "clv.mfile"
-    # _inherit = 'clv.mfile', 'clv.code02.model'
-    _inherit = 'clv.mfile'
+    _inherit = 'clv.mfile', 'clv.abstract.code02'
 
-    # code = fields.Char(string='Media File Code', required=False, default='/')
-    # code_sequence_03 = fields.Char(default='clv.mfile.code_03')
-    # code_sequence_04 = fields.Char(default='clv.mfile.code_04')
-    # code_sequence_06 = fields.Char(default='clv.mfile.code_06')
-    # code_sequence_09 = fields.Char(default='clv.mfile.code_09')
-    # code_sequence_10 = fields.Char(default='clv.mfile.code_10')
+    code = fields.Char(string='Media File Code', required=False, default='/')
+    code_sequence_03 = fields.Char(default='clv.mfile.code_03')
+    code_sequence_04 = fields.Char(default='clv.mfile.code_04')
+    code_sequence_06 = fields.Char(default='clv.mfile.code_06')
+    code_sequence_09 = fields.Char(default='clv.mfile.code_09')
+    code_sequence_10 = fields.Char(default='clv.mfile.code_10')
 
     @api.one
     def _compute_path_str(self):
