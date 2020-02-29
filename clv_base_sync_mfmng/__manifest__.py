a0ccf8d07e865da171b841f69e3acd21c180c411
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-02-29 17:43:56 -0300

    [IMP] clv_base_sync_mfmng (IMP __manifest__.py)

--- clv_base_sync_mfmng/__manifest__.py
+++ clv_base_sync_mfmng/__manifest__.py
@@ -11,7 +11,7 @@
     'license': 'AGPL-3',
     'website': 'https://github.com/CLVsol',
     'depends': [
-        'base',
+        'clv_base_mfmng',
         'clv_external_sync',
     ],
     'data': [
